53c13c3ca9061c711f2000275045bc17427b003c
Nguyen Quang Huy <quanghuy190105@gmail.com>
2026-02-25 13:48:19 +0700

    change some code in services, update requirements.txt, etc...

--- src/services/LLMService.py
+++ src/services/LLMService.py
@@ -1,19 +1,30 @@
 from langchain_ollama import ChatOllama
+from langchain_classic.chains import history_aware_retriever
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from repositories.LLMRepository import LLMRepository
 from domain.schema.Ai import ChatRequest
 from fastapi import Depends
+llm = ChatOllama(model="llama3.2")
+chat_history = []
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+    (
+        "system",
+        "You are an AI named Llama, a helpful assistant, Answer the user question simple and wisely.",
+    ),
+    MessagesPlaceholder(variable_name = "chat_history"),
+    ("human", "{input}"),
+    ]
+)
+chain = prompt_template | llm
 class AIService:
     def __init__(self, repo : LLMRepository = Depends()):
         self.repo = repo
-        self.llm = ChatOllama(model="llama3.2", temperature=0.5)
     async def ask(self, request: ChatRequest):
-        messages = [
-        (
-            "system",
-            "You are a helpful assistant, Answer the user question wisely",
-        ),
-        ("human", request.prompt),
-        ]
-        generated = await self.llm.ainvoke(messages)
-        aiAnswer = generated.content
-        return aiAnswer
+        response = await chain.ainvoke({"input": request.prompt, "chat_history": chat_history})
+        chat_history.append(HumanMessage(content=request.prompt))
+        chat_history.append(response)
+        print(f"AI: {response.content}")
+        return response.content
+
